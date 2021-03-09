c53dce173a3180a31a63420d59efaeb71d0b927a
jswj30 <jswj30@naver.com>
2021-03-09 20:30:50 +0900

    6.2

--- 06/learn-express/app.js
+++ 06/learn-express/app.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
 
 dotenv.config();
 const app = express();
@@ -25,6 +27,37 @@ app.use(
     },
     name: "session-cookie",
   })
+);
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 upload 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  }
 );
 
 app.use((req, res, next) => {
